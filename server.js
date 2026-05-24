3355e4faeb5910812765a1a3ab6b93fd43a74410
yaoayao6696-lab <yaoayao6696@gmail.com>
2026-05-24 12:37:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 const DB_FILE = 'database.json';
 
